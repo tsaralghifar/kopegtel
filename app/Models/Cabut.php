9d68452b40b60c5e02b74d959a1771dfc321662b
tsaralghifar <alghifaridev@gmail.com>
2021-04-07 14:24:12 +0700

    add model

---
+++ app/Models/Cabut.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cabut extends Model
+{
+    use HasFactory;
+}
